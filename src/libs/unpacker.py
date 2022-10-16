1e55536767d86c50b1ef4f04004e0530a72ffe35
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-16 01:45:01 -0300

    feat: Add ipv4 unpacking

--- src/libs/unpacker.py
+++ src/libs/unpacker.py
@@ -1,11 +1,28 @@
+import struct
+from libs.utils import Utils
+
 class Unpacker():
     @staticmethod
-    def unpack_ethernet(packet_data):
-        pass
+    def unpack_ipv4(data):
+        version_header_length = data[0]
 
-    @staticmethod
-    def unpack_ipv4(ethernet_data):
-        pass
+        version = version_header_length >> 4
+        header_length = version_header_length & 15
+
+        ttl, protocol, source, target = struct.unpack("! 8x B B 2x 4s 4s", data[:20])
+
+        print(len(data), header_length)
+
+        output = {
+            "version": version,
+            "ttl":  ttl,
+            "protocol":  Utils.format_protocol(protocol),
+            "source": Utils.format_ipv4(source),
+            "target": Utils.format_ipv4(target),
+            "payload": data[header_length:],
+        }
+
+        return output
 
     @staticmethod
     def unpack_tcp(ipv4_data):
