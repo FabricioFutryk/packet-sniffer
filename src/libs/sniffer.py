0c494d0dfc0507053ac8951907a5e8d9a2857d6b
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-16 00:31:53 -0300

    feat: Add initial classes

---
+++ src/libs/sniffer.py
@@ -0,0 +1,12 @@
+class PacketSniffer():
+    def __init__(self):
+        pass
+
+    def config(self):
+        pass
+
+    def start_sniffing(self):
+        pass
+
+    def log(self, info):
+        pass
---
+++ src/libs/unpacker.py
@@ -0,0 +1,20 @@
+class Unpacker():
+    @staticmethod
+    def unpack_ethernet(packet_data):
+        pass
+
+    @staticmethod
+    def unpack_ipv4(ethernet_data):
+        pass
+
+    @staticmethod
+    def unpack_tcp(ipv4_data):
+        pass
+
+    @staticmethod
+    def unpack_udp(ipv4_data):
+        pass
+
+    @staticmethod
+    def unpack_icmp(ipv4_data):
+        pass
