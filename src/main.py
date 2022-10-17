cbe7088a0340cf46e4d45d7e1f23de5ff3aff64f
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-17 00:30:48 -0300

    feat: Add cli args

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 from libs.sniffer import PacketSniffer
+import argparse
 
-sniffer = PacketSniffer()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="packet-sniffer", description="Capture network packets")
 
-sniffer.start_sniffing()
+    parser.add_argument('--filter', '-f', type=str,
+                        help="Filter the packets that you want to be shown. Ex: TCP/UDP/ICMP")
+    parser.add_argument('--sourcePorts', '-sP', type=str,
+                        help="Filter the packets with specified source port. Ex: 25565 or 0:443")
+    parser.add_argument('--destinationPorts', '-dP', type=str,
+                        help="Filter the packets with specified destination port. Ex: 25565 or 0:443")
+
+    args = parser.parse_args()
+
+    sniffer = PacketSniffer(vars(args))
+    sniffer.start_sniffing()
