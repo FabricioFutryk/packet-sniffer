50e4dc67d012448bd2a8236287536c7a22c34f3f
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-17 00:31:08 -0300

    feat: Add protocol to color method

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -1,3 +1,6 @@
+from colorama import Fore
+
+
 class Utils():
     @staticmethod
     def format_ipv4(address):
@@ -12,3 +15,13 @@ class Utils():
         }
 
         return protocols[protocol]
+
+    @staticmethod
+    def protocol_to_color(protocol):
+        colors = {
+            "TCP": Fore.LIGHTBLUE_EX,
+            "UDP": Fore.RED,
+            "ICMP": Fore.YELLOW,
+        }
+
+        return colors[protocol]
