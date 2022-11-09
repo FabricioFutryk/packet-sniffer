37e52bcdfe102ed78fa6bfa72b2aa9ad7a1b6c30
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-09 03:49:33 -0300

    refactor: Add gui and threaded sniffing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from libs.sniffer import PacketSniffer
-import argparse
+from gui.main import GUI
 import ctypes
 import sys
+from threading import Thread
 
 
 def is_admin():
@@ -12,21 +13,14 @@ def is_admin():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="packet-sniffer", description="Capture network packets")
-
-    parser.add_argument('--filter', '-f', type=str,
-                        help="Filter the packets that you want to be shown. Ex: TCP/UDP/ICMP")
-    parser.add_argument('--sourcePorts', '-sP', type=str,
-                        help="Filter the packets with specified source port. Ex: 25565 or 0:443")
-    parser.add_argument('--destinationPorts', '-dP', type=str,
-                        help="Filter the packets with specified destination port. Ex: 25565 or 0:443")
-
-    args = parser.parse_args()
-
     if is_admin():
-        sniffer = PacketSniffer(vars(args))
-        sniffer.start_sniffing()
+        gui = GUI()
+        sniffer = PacketSniffer(gui)
+
+        sniffer_thread = Thread(target=sniffer.start_sniffing)
+        sniffer_thread.start()
+
+        gui.render()
     else:
         ctypes.windll.shell32.ShellExecuteW(
             None, "runas", sys.executable, " ".join(sys.argv), None, 1)
