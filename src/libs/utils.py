885d0a28c8c7fdcdeba4fcd768ff5ccd0c4a33b9
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-10 16:44:10 -0300

    refactor: Remove protocol to color method

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -1,6 +1,3 @@
-from colorama import Fore
-
-
 class Utils():
     @staticmethod
     def format_ipv4(address):
@@ -15,13 +12,3 @@ class Utils():
         }
 
         return protocols[protocol]
-
-    @staticmethod
-    def protocol_to_color(protocol):
-        colors = {
-            "TCP": Fore.LIGHTBLUE_EX,
-            "UDP": Fore.RED,
-            "ICMP": Fore.YELLOW,
-        }
-
-        return colors[protocol]
