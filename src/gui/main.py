a85419e912076657ef0c03a2f02970ead5286e73
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-10 16:45:44 -0300

    feat: Add notebook for log and statistics

--- src/gui/main.py
+++ src/gui/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import ttk
+from gui.packet_log import PacketLog
+from gui.statistics import Statistics
 
 
 class GUI(tk.Tk):
@@ -7,24 +9,19 @@ class GUI(tk.Tk):
         super().__init__()
 
         self.title("Capturador de paquetes de Red")
-        self.geometry("475x220")
+        self.geometry("480x220")
         self.resizable(False, False)
         self.config(padx=10, pady=10)
 
-        self.text = tk.Text(self, state=tk.DISABLED)
+        self.notebook = ttk.Notebook(self)
 
-        self.text.pack()
+        self.packet_register = PacketLog(self)
+        self.statistics = Statistics(self)
 
-        self.text.tag_config('TCP', foreground='blue')
-        self.text.tag_config('UDP', foreground='red')
-        self.text.tag_config('ICMP', foreground='orange')
+        self.notebook.add(self.packet_register, text="Registro")
+        self.notebook.add(self.statistics, text="Estadisticas")
 
-        self.text.tag_config(
-            'activeFlag', background="green", foreground='white')
-        self.text.tag_config(
-            'disabledFlag', background="red", foreground='white')
-
-        self.text.bind("<1>", lambda event: self.text.focus_set())
+        self.notebook.pack()
 
     def render(self):
         self.mainloop()
