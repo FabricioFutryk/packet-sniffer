11e38071d0253c4a750d9fe3cf1a5613e4a17a1c
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 18:45:49 -0300

    feat: Add packet count

--- src/gui/statistics.py
+++ src/gui/statistics.py
@@ -10,12 +10,18 @@ class Statistics(tk.Frame):
 
         self.maxTTL = None
         self.minTTL = None
+        self.packet_count = 0
 
         self.minTTL_text = tk.StringVar(value='Min. TTL: 0')
         self.maxTTL_text = tk.StringVar(value='Max. TTL: 0')
+        self.packet_count_text = tk.StringVar(value='N° Paquetes: 0')
 
-        tk.Label(self, textvariable=self.maxTTL_text).place(x="375", y="10")
-        tk.Label(self, textvariable=self.minTTL_text).place(x="375", y="30")
+        tk.Label(self, textvariable=self.packet_count_text).place(
+            x="445", y="10", anchor=tk.E)
+        tk.Label(self, textvariable=self.maxTTL_text).place(
+            x="445", y="30", anchor=tk.E)
+        tk.Label(self, textvariable=self.minTTL_text).place(
+            x="445", y="50", anchor=tk.E)
 
         self.originsFrame = tk.LabelFrame(
             self, text="Orígenes", padx=5, pady=2)
@@ -33,6 +39,7 @@ class Statistics(tk.Frame):
     def refresh(self):
         self.minTTL_text.set(f'Min. TTL: {self.minTTL}')
         self.maxTTL_text.set(f'Max. TTL: {self.maxTTL}')
+        self.packet_count_text.set(f'N° Paquetes: {self.packet_count}')
 
         self.origins.delete(0, tk.END)
         self.origins.insert(0, *self.originsValues)
