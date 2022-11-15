fe0f8ea2ef72a368775adefc9e1bd5461fb30e06
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-15 18:44:50 -0300

    feat: Handle close

--- src/gui/main.py
+++ src/gui/main.py
@@ -2,11 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from gui.packet_log import PacketLog
 from gui.statistics import Statistics
+import sys
+from tkinter import messagebox
 
 
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        self.is_open = True
 
         self.title("Capturador de paquetes de Red")
         self.geometry("480x220")
@@ -22,6 +26,14 @@ class GUI(tk.Tk):
         self.notebook.add(self.statistics, text="Estadisticas")
 
         self.notebook.pack()
+
+        self.protocol('WM_DELETE_WINDOW', self.close)
+
+    def close(self):
+        if messagebox.askokcancel('Salir', 'Está seguro que desea salir del programa?'):
+            self.destroy()
+            sys.exit()
+            self.is_open = False
 
     def render(self):
         self.mainloop()
